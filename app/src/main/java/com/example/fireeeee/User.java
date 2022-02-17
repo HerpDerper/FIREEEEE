e683f73e84e8036f4f314614ce2fe7b33f954cb5
eduardkutermin@gmail.com <eduardkutermin@gmail.com>
2022-02-18 00:06:35 +0300

    first commit

---
+++ app/src/main/java/com/example/fireeeee/User.java
@@ -0,0 +1,13 @@
+package com.example.fireeeee;
+
+public class User {
+    public String phone;
+    public String email;
+    public String password;
+
+    public User(String phone, String email, String password){
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+}
